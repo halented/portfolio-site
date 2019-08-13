dea193e6e51f554f40f4496d88a6a3afc265be2c
Hallie Dunn <hal.dunn@flatironschool.com>
2019-08-13 13:46:40 -0700

    Updates

---
+++ precache-manifest.62ec72e1c2d08ffe96b0d240c73f567e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a5910c2d69a4a159076ba01594c208",
+    "url": "/index.html"
+  },
+  {
+    "revision": "265626829c17de4c2dd5",
+    "url": "/static/css/main.bd0d177d.chunk.css"
+  },
+  {
+    "revision": "37ec18df8c768d71205b",
+    "url": "/static/js/2.bca6dc5a.chunk.js"
+  },
+  {
+    "revision": "265626829c17de4c2dd5",
+    "url": "/static/js/main.e693ed5b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "060ec8b688073efb35a400999c2bb4ac",
+    "url": "/static/media/closeup.060ec8b6.png"
+  },
+  {
+    "revision": "2649defe625da3bb301529b2d39e763b",
+    "url": "/static/media/die.2649defe.png"
+  },
+  {
+    "revision": "4402db074505dca61291e0d955e2ae7e",
+    "url": "/static/media/dog1.4402db07.png"
+  },
+  {
+    "revision": "795064bbb7142b324d2b8209c5f7e7b6",
+    "url": "/static/media/dog2.795064bb.png"
+  },
+  {
+    "revision": "9a890fbd060151c7b71cf0f5bdbb3ef7",
+    "url": "/static/media/dog3.9a890fbd.png"
+  },
+  {
+    "revision": "f363caceca8b16b4bcc056ec36fe267d",
+    "url": "/static/media/dog4.f363cace.png"
+  },
+  {
+    "revision": "7b9cfaed012ecab2bf6560ea37649110",
+    "url": "/static/media/dog5.7b9cfaed.png"
+  },
+  {
+    "revision": "bd9f6087fd3e1f111b9b38864734fae6",
+    "url": "/static/media/dog6.bd9f6087.png"
+  },
+  {
+    "revision": "349c6faee68e5a2747e8bf09ecb38cf7",
+    "url": "/static/media/dog7.349c6fae.png"
+  },
+  {
+    "revision": "f96b0e91bc2a9214c98055073fbd74e5",
+    "url": "/static/media/dog8.f96b0e91.png"
+  },
+  {
+    "revision": "ae19cce8771139e9dc5dabc4b5d657b7",
+    "url": "/static/media/javathescript.ae19cce8.png"
+  },
+  {
+    "revision": "a1d4caa8efca90e712bb73a21ebb872c",
+    "url": "/static/media/twit.a1d4caa8.png"
+  }
+]);

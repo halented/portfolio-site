c623c0be63612a8ee16e60b80c651bbd15fa2106
Hallie Dunn <hallie.dunn1@gmail.com>
2020-05-27 10:27:13 -0700

    refactored to remove..just...bad code

--- src/comps/Projects.js
+++ src/comps/Projects.js
@@ -2,69 +2,56 @@ import React, { Component } from 'react';
 import Nav from './Nav';
 import twit from '../images/twit.png';
 import die from '../images/die.png';
-import icon from '../images/icon.png';
+import final from '../images/finally.png';
+import { projects } from '../projectData.json'
 
 class Projects extends Component {
 
+  state = {
+    showProject: false,
+    selectedProject: {},
+    image: null
+  }
+
   populateDeets = (n) => {
-    let prev = document.getElementById('prev')
-    prev.style = {display: ''}
-    prev.innerText = ''
-    let img = document.createElement('img')
-    img.id = 'prevImage'
-    img.style = 'margin: auto; margin-bottom: 10%;'
-    let a1 = document.createElement('a')
-    let a2 = document.createElement('a')
-    a1.target = '_blank'
-    a2.target = '_blank'
-    let p = document.createElement('p')
-    let br = document.createElement('br')
-    let br2 = document.createElement('br')
-    if(n===1){
-      img.src = icon
-      a1.innerText = 'Github Repo'
-      a1.href= 'https://github.com/halented/finally-frontend'
-      a2.innerText = "Live Demo"
-      a2.href = 'https://finally-app.herokuapp.com/#/'
-      p.innerText = "Tester account easily accessible from the signup page"
+    let selectedProject = projects.find(proj=>proj.id ===n)
+    let image;
+    if(selectedProject.name === "DnDluxe"){
+      image = die
     }
-    else if(n===2){
-      img.src = die
-      a1.innerText = 'Github Repo'
-      a1.href= 'https://github.com/halented/dndeluxe-frontend'
-      a2.innerText = "Live Demo"
-      a2.href = 'http://dndluxe.herokuapp.com/'
-      p.innerText = "Tester account username: 'maeCat' / password: 'woods'. Account information is case sensitive."
+    else {
+      selectedProject.name === "Finally!" ? image = final : image = twit
     }
-    else if(n===3){
-      img.src = twit
-      a1.innerText = 'Github Repo'
-      a1.href= 'https://github.com/halented/twitflip'
-      a2.innerText = "Live Demo"
-      a2.href = 'http://twitflip.herokuapp.com/'
-      p.innerText = "Tester account username: 'tester'. Account information is case sensitive."
-    }
-    prev.appendChild(img)
-    prev.appendChild(br2)
-    prev.appendChild(a1)
-    prev.appendChild(br)
-    prev.appendChild(a2)
-    prev.appendChild(p)
+    this.setState({showProject: true, selectedProject, image})
+  }
+  
+  populateProjects = () => {
+    return projects.map(project=>{
+      return <li onClick={()=>this.populateDeets(project.id)}><strong>{project.name}</strong> {project.sideDesc}</li>
+    })
   }
 
   render(){
+    const {showProject, image, selectedProject} = this.state
     return (
       <>
       <div className='head'>Projects</div>
       <div className='displayBox'>
         <div id='projectWheel'> 
           <ul>
-            <li onClick={()=>this.populateDeets(1)}><strong>Finally!</strong> - An app for extroverts to manage their urges to ask introverted friends to hangout</li>
-            <li onClick={()=>this.populateDeets(2)}><strong>DnDluxe</strong> - A web app where a user can store information about DnD characters and games.</li>
-            <li onClick={()=>this.populateDeets(3)}><strong>TwitFlip</strong> - A Twitter surfing web-app. Users can search and save tweets, and use the positive/negative attitude filter.</li>
+            {this.populateProjects()}
           </ul> 
         </div>
-        <div id='prev'>Click a project for more information!</div>
+        {showProject ? 
+          <div id='prev' style={{display: 'flex', flexDirection: 'column'}}>
+            <img id='prevImage' src={image} alt='project icon'></img>
+            <a target="_blank" rel="noopener noreferrer" href={selectedProject.repo}>Github Repo</a>
+            <a target="_blank" rel="noopener noreferrer" href={selectedProject.demo}>Live Demo</a>
+            <p>{selectedProject.testerInfo}</p>
+          </div>
+        :
+          <div id='prev'>Click a project for more information!</div>
+        }
       </div>
       <Nav/>
       </>
@@ -73,11 +60,3 @@ class Projects extends Component {
 }
 
 export default Projects;
-
-// img.src = java
-//       img.style = 'margin: auto; margin-bottom: 10%; margin-top: 20%;'
-//       a1.innerText = 'Github Repo'
-//       a1.href= 'https://github.com/halented/One_Page_RPG_Frontend'
-//       a2.innerText = "Video Demo"
-//       a2.href = 'https://www.youtube.com/watch?v=KkO_6orX0ow&feature=youtu.be'
-//       p.innerText = "Please clone both front and backend repositories to play the game locally!"

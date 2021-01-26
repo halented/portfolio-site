3749be5ca8b006f202ec318aba387fef05a905c9
Hal Dunn <heyhal19@gmail.com>
2021-01-26 14:19:50 -0800

    update resume link

--- src/comps/Nav.js
+++ src/comps/Nav.js
@@ -10,7 +10,7 @@ function Nav() {
                 <Link to='/home' className='links'>About</Link>
                 <Link to='/projects' className='links'>Projects</Link>
                 <Link to='/contact' className='links'>Contact</Link>
-                <a href="https://www.canva.com/design/DADzi9s9gW8/GNswvIF4os0mdqZKFSNokw/view?utm_content=DADzi9s9gW8&utm_campaign=designshare&utm_medium=link&utm_source=viewer" target="_blank" rel="noopener noreferrer" className='links'>Resume</a>
+                <a href="https://www.canva.com/design/DAEURCynL2w/7MzSvI2MK6ElONcCjpZs6g/view?utm_content=DAEURCynL2w&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" target="_blank" rel="noopener noreferrer" className='links'>Resume</a>
             </div>
             </>
         )

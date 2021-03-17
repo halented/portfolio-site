c1fd2ee9e916e7d50ba51e0e4cd97520bf9616cf
Hal Dunn <heyhal19@gmail.com>
2021-03-17 10:12:09 -0700

    logo change

--- src/pages/bones/Buttons.js
+++ src/pages/bones/Buttons.js
@@ -1,17 +1,21 @@
 import { Button } from 'antd'
 import { styles } from '../../styles'
 import { Link } from 'react-router-dom'
+import h from '../../images/aytch-mt.png'
+
+const btnStyle = { backgroundColor: '#9ac6c8ff', fontWeight: 'bold', marginTop: '10%' }
 
 function Buttons() {
     return (
         <div style={styles.buttonBox}>
             <Link to='/projects'>
-                <Button style={{ backgroundColor: '#9ac6c8ff', fontWeight: 'bold' }}>
+                <Button style={btnStyle}>
                     PROJECTS
                 </Button>
             </Link>
+            <img src={h} alt='logo of an H' />
             <Link to='contact'>
-                <Button style={{ backgroundColor: '#9ac6c8ff', fontWeight: 'bold' }}>
+                <Button style={btnStyle}>
                     CONTACT
                 </Button>
             </Link>
